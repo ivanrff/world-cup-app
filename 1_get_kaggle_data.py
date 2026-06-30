c58dd51cea1c52f68fde6bcb2a32d6a32af21a3b
eng.ivanrff <eng.ivanrff@gmail.com>
2026-06-30 17:52:34 -0300

    structuring

---
+++ 1_get_kaggle_data.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+import kagglehub
+
+# load_dotenv()
+# KAGGLE_API_TOKEN = os.getenv("KAGGLE_API_TOKEN")
+
+files = ['matches.csv', 'teams.csv']
+
+for file in files:
+    kagglehub.dataset_download(
+        'mominullptr/fifa-world-cup-2026-dataset',
+        path=file,
+        output_dir='data',
+        force_download=True)
