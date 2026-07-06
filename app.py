875ff0e5cd3ce3273b1b709ef7853a4f47686b0e
eng.ivanrff <eng.ivanrff@gmail.com>
2026-07-06 15:07:07 -0300

    editing the dashboard

--- app.py
+++ app.py
@@ -15,17 +15,20 @@ def load_data():
     conn.close()
     
     # Garantir que a coluna de snapshot seja datetime
-    df['snapshot'] = pd.to_datetime(df['snapshot'])
+    df['snapshot_br'] = pd.to_datetime(df['snapshot_br'])
     return df
 
-df = load_data()
+df = load_data().sort_values(by='final_whistle_br')
+df = df[(df['match_status'] != "TBD")].copy()
+# df_future = df[(df['match_status'] != "TBD") & (df['match_status'] != "Played")]
+# df_past = df[df['match_status'] == "Played"]
 
 # --- Sidebar para filtros ---
 st.sidebar.header("Filtros")
-selected_stage = st.sidebar.multiselect("Selecione a fase:", df['stage'].unique())
+selected_snapshot = st.sidebar.selectbox("Selecione o snapshot:", (df['snapshot_br'].sort_values().unique()))
 
-if selected_stage:
-    df_filtered = df[df['stage'].isin(selected_stage)]
+if selected_snapshot:
+    df_filtered = df[df['snapshot_br'] <= selected_snapshot]
 else:
     df_filtered = df
 
@@ -34,21 +37,32 @@ st.title("📊 Painel de Previsões da Opta")
 
 # Exibir os dados em tabela (usando o data_editor para permitir ordenação)
 st.subheader("Snapshot mais recente")
-st.dataframe(df_filtered.sort_values(by='snapshot', ascending=False))
+st.dataframe(df_filtered.sort_values(by='final_whistle_br'))
 
 # --- Exemplo de Gráfico: Evolução das Probabilidades ---
-st.subheader("Evolução das Previsões por Jogo")
-match_ids = df_filtered['opta_match_id'].unique()
-selected_match = st.selectbox("Escolha um jogo para ver a tendência:", match_ids)
-
-if selected_match:
-    match_data = df_filtered[df_filtered['opta_match_id'] == selected_match]
     
-    # Criar um gráfico de linhas simples com Plotly
-    fig = px.line(
-        match_data, 
-        x='snapshot', 
-        y=['home_proba', 'away_proba', 'draw_proba'],
-        title=f"Tendência de probabilidade - Jogo {selected_match}"
-    )
-    st.plotly_chart(fig, use_container_width=True)
+# Criar um gráfico de linhas simples com Plotly
+fig = px.line(
+    df_filtered, 
+    x='final_whistle_br', 
+    y=['model_grade'],
+    title=f"Desempenho do modelo"
+)
+st.plotly_chart(fig, use_container_width=True)
+
+# # --- Exemplo de Gráfico: Evolução das Probabilidades ---
+# st.subheader("Evolução das Previsões por Jogo")
+# match_ids = df_filtered['match_handle'].unique()
+# selected_match = st.selectbox("Escolha um jogo para ver a tendência:", match_ids)
+
+# if selected_match:
+#     match_data = df_filtered[df_filtered['match_handle'] == selected_match]
+    
+#     # Criar um gráfico de linhas simples com Plotly
+#     fig = px.line(
+#         match_data, 
+#         x='snapshot_br', 
+#         y=['home_proba', 'away_proba', 'draw_proba'],
+#         title=f"Tendência de probabilidade - Jogo {selected_match}"
+#     )
+#     st.plotly_chart(fig, use_container_width=True)
--- src/4_db_ingest.py
+++ src/4_db_ingest.py
@@ -4,13 +4,19 @@ import json
 import pandas as pd
 import sqlite3
 
+
+def to_br_timezone(column):
+    converted_col = column.dt.tz_convert(
+    "America/Sao_Paulo").dt.tz_localize(None)
+
+    return converted_col
+
 fixture_files = [f.name for f in Path('../data/opta/fixture_jsons').iterdir() if f.is_file()]
 fixture_files.sort()
 
 dfs_list = []
-# print(fixture_files)
 for file in fixture_files:
-    # print(file)
+
     snapshot = file.split('f')[0][1:][:-1]
     with open(f"../data/opta/fixture_jsons/{file}", 'r', encoding='utf-8') as f:
         data = json.load(f)
@@ -20,16 +26,12 @@ for file in fixture_files:
 
         match_id = id
 
-        # if match_id == 'b083yrj18tbck1z0og2evbhuc':
-        #     break
-
         match = data[match_id]
 
         checker = {}
 
         checker['opta_match_id'] = id
-        checker['snapshot'] = pd.to_datetime(snapshot, format="%Y%m%d%H%M%S")
-        # checker['match_id'] = i + 1
+        checker['snapshot_br'] = pd.to_datetime(snapshot, format="%Y%m%d%H%M%S")
 
         matchInfo = match.get("matchInfo", None)
 
@@ -44,8 +46,6 @@ for file in fixture_files:
 
         if checker['stage'].lower() == 'group stage':
             checker['group'] = matchInfo['series'].get('name')
-        # else:
-        #     checker['group'] = None
         
         for team in range(2):
             position = matchInfo['contestant'][team].get('position')
@@ -64,6 +64,12 @@ for file in fixture_files:
         checker['match_status'] = match_results['matchStatus']
 
         if checker['match_status'] == 'Played':
+
+            for period in match_results['period']:
+                if period['id'] == len(match_results['period']):
+                    checker['final_whistle'] = pd.to_datetime(period['end'])
+
+
             checker['home_score'] = match_results['scores']['ft']['home']
             checker['away_score'] = match_results['scores']['ft']['away']
             if checker['home_score'] > checker['away_score']:
@@ -90,28 +96,33 @@ for file in fixture_files:
 
 final_df = pd.concat(dfs_list, ignore_index=True)
 
+# deixando as colunas lower case para consistencia
 final_df.columns = [col.lower() for col in final_df.columns]
 
+# colocando as coluans numéricas como números
 for col in ['home_proba', 'away_proba', 'draw_proba', 'home_score', 'away_score']:
     final_df[col] = pd.to_numeric(final_df[col])
 
-final_df['match_datetime_br'] = final_df["match_datetime"].dt.tz_convert(
-    "America/Sao_Paulo").dt.tz_localize(None)
+# Convertendo as datas para horario de brasilia
+final_df['match_datetime_br'] = to_br_timezone(final_df["match_datetime"])
+final_df['final_whistle_br'] = to_br_timezone(final_df["final_whistle"])
+final_df = final_df.drop(columns=['match_datetime', 'final_whistle'])
 
-final_df = final_df.drop(columns=['match_datetime'])
-
+# criando a coluna de handles. Ex.: 'BRA x FRA'
 final_df['match_handle'] = final_df['home_code'] + ' x ' + final_df['away_code']
 
+# removendo as linhas repetidas de partidas já jogadas
 played_df = final_df[final_df['match_status'].isin(['Played', 'TBD'])]
 other_df = final_df[~final_df['match_status'].isin(['Played', 'TBD'])]
-played_df_clean = played_df.drop_duplicates(subset=[col for col in final_df.columns if col != 'snapshot'], keep='last')
-test = pd.concat([played_df_clean, other_df])
-test = test.sort_values(by=['match_datetime_br', 'match_handle', 'snapshot'])
-test.to_csv("../test.csv", index=False)
+final_df_clean = played_df.drop_duplicates(subset=[col for col in final_df.columns if col != 'snapshot_br'], keep='last')
+final_df_clean = pd.concat([final_df_clean, other_df])
+
+# salvando para visualizar em csv
+final_df_clean.sort_values(by=['match_datetime_br', 'final_whistle_br', 'snapshot_br']).to_csv("../test.csv", index=False)
 
 conn = sqlite3.connect("../data/db/world_cup.db")
 
-final_df.to_sql(
+final_df_clean.to_sql(
     name="opta_snapshots",  # Nome da tabela dentro do SQLite
     con=conn,  # A conexão que abrimos acima
     if_exists="replace",  # 'append' adiciona os dados novos. 'replace' reconstrói a tabela do zero.
