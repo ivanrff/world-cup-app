3cebb591a8898fb5e29045bfdf453e054208b875
eng.ivanrff <eng.ivanrff@gmail.com>
2026-07-09 15:27:37 -0300

    some changes

--- src/1_get_kaggle_data.py
+++ src/1_get_kaggle_data.py
@@ -11,5 +11,5 @@ for file in files:
     kagglehub.dataset_download(
         'mominullptr/fifa-world-cup-2026-dataset',
         path=file,
-        output_dir='data/kaggle',
+        output_dir='../data/kaggle',
         force_download=True)
