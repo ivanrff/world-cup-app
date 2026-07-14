3b50ba5b9d50dc1bb8c1f94edf40f51a5d46a5f8
eng.ivanrff <eng.ivanrff@gmail.com>
2026-07-14 15:56:07 -0300

    locale br failed on streamlit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 from datetime import datetime, timedelta
 import locale
 
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+# locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 # Configuração da página para ficar larga e aproveitar melhor o espaço
 st.set_page_config(layout="wide", page_title="Previsões do \"Supercomputador\" Opta")
